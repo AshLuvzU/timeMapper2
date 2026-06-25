dbc5045306c044b0c2f77b4ebb01ee998f2cc72f
AshLuvzU <hkm5375@psu.edu>
2026-06-25 15:31:43 -0400

    fixed markers and added content for tutorial

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,16 @@
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
 import markerShadow from 'leaflet/dist/images/marker-shadow.png'
  
 // Fix Leaflet's default marker icon paths, which break under Vite
 delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
   shadowUrl: markerShadow,
 })
- 
 // ---------------------------------------------------------------------------
 // getChild()
 // Helper: finds a direct child element by tag name.
